1c0fafdea80ec6b62f8b2b32afa470815da7d6cb
Abraham Matus <aimatusco@gmail.com>
2021-01-14 17:22:28 -0500

    adds spy example

--- src/sum.js
+++ src/sum.js
@@ -1,4 +1,7 @@
+import * as emailUtil from './email-util/emailUtil';
+
 const sum = (a, b) => {
+    emailUtil.sendEmail('a@a.com', 'Hello!', 'Here be dragons');
     return a + b;
 };
 
--- src/sum.test.js
+++ src/sum.test.js
@@ -1,5 +1,14 @@
 import sum from './sum';
+import * as emailUtil from './email-util/emailUtil';
 
 test('adds 1+2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+    // Arrange
+    const spy = jest.spyOn(emailUtil, "sendEmail");
+
+    // Act
+    const result = sum(1, 2);
+
+    // Assert
+    expect(result).toBe(3);
+    expect(spy).toHaveBeenCalled();
 });
